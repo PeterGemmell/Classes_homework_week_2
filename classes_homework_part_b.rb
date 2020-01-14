56d9b571f6e8ead2bc4af0d20225b11746030bde
Peter Gemmell <petergemmell@Peters-MacBook-Air.local>
2020-01-14 08:54:41 +0000

    homework part b complete

--- classes_homework_part_b.rb
+++ classes_homework_part_b.rb
@@ -1,4 +1,64 @@
 class SportsTeam
-  def initialize()
+
+  attr_reader :team_name, :players, :coach, :team_points
+  attr_writer :team_name, :players, :coach, :team_points
+  # attr_accessor :team_name, :players, :coach
+
+  def initialize(input_team_name, input_players, input_coach, input_team_points)
+    @team_name = input_team_name
+    @players = input_players
+    @coach = input_coach
+    @team_points = input_team_points
+
+    end
+#1
+def team_name()
+  return @team_name
+end
+
+#2
+def players()
+  return @players
+end
+
+#3
+def coach()
+  return @coach
+end
+
+#Setter for changing coach name.
+
+def set_coach(new_coach)
+  @coach = new_coach
+end
+
+#Adding new player to player array:
+
+def new_player(player)
+  player = @players.push
+end
+
+#Add a method that takes in a string of a players name.Checks to see if They
+#are in players array.
+def player_name(player)
+  if player == @sports_team.include?("David")
+    return true
+
   end
+end
+
+
+#Takes in whether the team has won or lost and updates the points
+#property for a win.
+def team_won_lost(points)
+  total = 0
+  for points in @team_points
+    total += points[:team_points]
+    return total
+  end
+end
+
+
+
+
 end
--- specs/classes_homework_part_b_specs.rb
+++ specs/classes_homework_part_b_specs.rb
@@ -7,5 +7,52 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class TestSportsTeam < MiniTest::Test
 
   def setup
+    @sports_team = SportsTeam.new("Yankees",["Peter","David","John","Andrew"],"Brian","5")
+
   end
+
+  #   #Test Getters:
+  def test_team_name
+
+    assert_equal("Yankees", @sports_team.team_name)
+  end
+
+  #2
+ def test_players
+
+   assert_equal(["Peter","David","John","Andrew"], @sports_team.players)
+ end
+
+ #3
+ def test_coach
+
+   assert_equal("Brian", @sports_team.coach)
+ end
+
+ #Test Setters:
+ def test_set_coach
+   @sports_team.coach = "Steven"
+   assert_equal("Steven", @sports_team.coach)
+ end
+
+ #Adding a new player to player array:
+ def test_new_player
+   @sports_team.players = "Bob"
+   assert_equal("Bob", @sports_team.players)
+ end
+
+ #Add a method that takes in a string of a players name.Checks to see if They
+ #are in players array.
+ def test_player_name
+   @sports_team.players = "David"
+   assert_equal("David", @sports_team.players)
+ end
+
+ def test_team_won_lost
+
+   assert_equal("5", @sports_team.team_points)
+ end
+
+
+
 end
